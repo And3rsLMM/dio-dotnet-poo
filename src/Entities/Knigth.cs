0d6c6fc634d7e09e7cc059f7359ef86a2740a893
And3rsLMM <andersonlimam.al@gmail.com>
2022-03-09 23:11:48 -0300

    Adicionar arquivos de projeto.

---
+++ src/Entities/Knigth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dio_dotnet_poo.src.Entities
+{
+    internal class Knight : Hero
+    {
+        public Knight(string Name, int Level, string Herotype)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using dio_dotnet_poo.src.Entities;
+
+namespace dio_dotnet_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23,"Knight");
+            Wizard jennica = new Wizard("Jennica", 23, "Wizard");
+            
+            Console.WriteLine(arus.Attack());
+            Console.WriteLine(jennica.Attack(1));
+            Console.WriteLine(jennica.Attack(7));
+        }
+    }
+}
